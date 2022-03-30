8797f1faf686a8c5669a42d59b97b83d51b2b80f
rojenzaman <rojen@riseup.net>
2022-03-30 19:14:56 +0300

    update run

---
+++ exec.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# replace tag and OCI in ENV if you want
+. ENV &>/dev/null
+
+if [[ ! "$(${OCI} ps -a | grep "amusewiki_${TAG}")" ]]; then
+	${OCI} run -d --network host --name amusewiki_${TAG} rojen/amusewiki:${TAG}
+fi
+
+if [[ ! "$(${OCI} ps| grep "amusewiki_${TAG}")" ]]; then
+	${OCI} start amusewiki_${TAG}
+fi
+
+${OCI} exec -it amusewiki_${TAG} /bin/bash
