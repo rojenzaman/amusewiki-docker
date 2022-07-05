4902278bbe8957c3cb4eee88969d0c2466bc95c6
rojenzaman <rojen@riseup.net>
2022-07-05 21:46:55 +0000

    create Dockerfile.almalinux

--- build.sh
+++ build.sh
@@ -3,16 +3,14 @@
 . ENV &>/dev/null
 
 case "${1}" in
-	Dockerfile.centos|centos) ${OCI} build -t rojen/amusewiki:centos -f Dockerfile.centos . ;;
 	Dockerfile.debian|debian) ${OCI} build -t rojen/amusewiki:debian -f Dockerfile.debian . ;;
 	Dockerfile.debian-texlive|debian-texlive|latest) ${OCI} build -t rojen/amusewiki:latest -t rojen/amusewiki:debian-texlive -f Dockerfile.debian-texlive . ;;
 	Dockerfile.fedora|fedora) ${OCI} build -t rojen/amusewiki:fedora -f Dockerfile.fedora . ;;
-	Dockerfile.rockylinux|rockylinux|rocky) ${OCI} build -t rojen/amusewiki:rockylinux -f Dockerfile.rockylinux . ;;
+	Dockerfile.almalinux|almalinux|alma) ${OCI} build -t rojen/amusewiki:almalinux -f Dockerfile.almalinux . ;;
 	*)
-		${OCI} build -t rojen/amusewiki:centos -f Dockerfile.centos .
 		${OCI} build -t rojen/amusewiki:debian -f Dockerfile.debian .
 		${OCI} build -t rojen/amusewiki:latest -t rojen/amusewiki:debian-texlive -f Dockerfile.debian-texlive .
 		${OCI} build -t rojen/amusewiki:fedora -f Dockerfile.fedora .
-		${OCI} build -t rojen/amusewiki:rockylinux -f Dockerfile.rockylinux .
+		${OCI} build -t rojen/amusewiki:almalinux -f Dockerfile.almalinux .
 	;;
 esac
