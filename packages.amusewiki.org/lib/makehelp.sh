3286c88a75b616ca69734c58e3fff425a1d36b91
rojenzaman <rojen@riseup.net>
2022-07-31 11:06:53 +0300

    fix make help

--- packages.amusewiki.org/lib/makehelp.sh
+++ packages.amusewiki.org/lib/makehelp.sh
@@ -3,6 +3,6 @@
 targets="$(make -rpn | sed -n -e '/^$/ { n ; /^[^ .#][^ ]*:/ { s/:.*$// ; p ; } ; }' | grep -v "\-\-" | sort -u)"
 
 while IFS= read -r target; do
-	targethelp="$(cat ".lib/makehelp/${target}" 2>/dev/null )"
+	targethelp="$(cat "lib/makehelp/${target}" 2>/dev/null )"
 	echo "+${target} +${targethelp}"
 done <<< "${targets}" | column -t -s '+' | grep -v default
