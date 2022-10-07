22db8d628e472f5a01b858470a2ae2e2f1460639
rojenzaman <rojen@riseup.net>
2022-10-07 22:04:49 +0300

    update ssh public key get method

--- texlive-base/lib/post-ssh.sh
+++ texlive-base/lib/post-ssh.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# volumes:
-#   - '${HOME}/.ssh/id_rsa.pub:/etc/host-ssh.d/authorized_keys'
 # ports:
 #   - '6022:22'
 # host ssh config:
@@ -9,7 +7,10 @@
 #   user amusewiki
 #   port 6022
 
-# import host public key
-if [ -f /etc/host-ssh.d/authorized_keys ]; then
-        cp /etc/host-ssh.d/authorized_keys /var/lib/amusewiki/.ssh
+# terminate if public key is not set
+if [ -z "${HOST_SSH_PUBLIC_KEY}" ]; then
+	exit 0
+else
+	# write ssh key
+	echo "${HOST_SSH_PUBLIC_KEY}" > /var/lib/amusewiki/.ssh/authorized_keys
 fi
